d8f402e5dd5129ca04338ef5b6e9cc46696765dc
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-06-27 00:29:15 +0000

    add clear cache and refresh in settings

--- packages/client/src/app/components/settings/settings.component.ts
+++ packages/client/src/app/components/settings/settings.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { THEMES_LIST, ThemeService } from 'src/app/modules/theme';
 import { Theme } from 'src/app/modules/theme/models';
 import { DataService } from 'src/app/services/data-service/data.service';
+import { GamesService } from 'src/app/services/game-service/games.service';
 
 @Component({
   selector: 'app-settings',
@@ -13,11 +14,16 @@ export class SettingsComponent {
   themes = THEMES_LIST;
   currentTheme: Observable<Theme>;
 
-  constructor (protected readonly themeService: ThemeService, protected readonly dataService: DataService) {
+  constructor (protected readonly themeService: ThemeService, protected readonly dataService: DataService, protected readonly gameService: GamesService) {
     this.currentTheme = themeService.currentTheme;
   }
 
   clearCache() {
     this.dataService.clearCache();
+  }
+
+  clearCacheAndReload() {
+    this.dataService.clearCache();
+    this.gameService.fetchGames();
   }
 }
