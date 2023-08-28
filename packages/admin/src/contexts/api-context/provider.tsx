6129e9414b75e1c0be84b447ae6310b4654803b8
Charles-François St-Cyr <charlesf2001@gmail.com>
2023-08-28 11:15:21 -0400

    remove envalid

--- packages/admin/src/contexts/api-context/provider.tsx
+++ packages/admin/src/contexts/api-context/provider.tsx
@@ -1,11 +1,10 @@
 import React, { PropsWithChildren } from 'react';
 import axios from 'axios';
 import { ApiContext } from './context';
-import { env } from '../../utils/environment';
 
 export const ApiProvider: React.FC<PropsWithChildren> = ({ children }) => {
     const instance = axios.create({
-        baseURL: env.REACT_APP_SERVER_URL,
+        baseURL: process.env.REACT_APP_SERVER_URL,
     });
 
     return (
